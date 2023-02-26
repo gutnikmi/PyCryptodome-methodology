b51cac350cfd04699fedac2fb8751f3b84af7ef3
gutnikmi <gutnikmi@mail.ru>
2023-02-26 16:20:26 +0300

    added rsa 1

--- code/Signer.py
+++ code/Signer.py
@@ -8,11 +8,6 @@ def sign_pkcs1(hash_chosen, key_source): #signer for Rsa PKCS#1 v1.5
         'generate': RSA.generate(2048)
     }
     key = keys[key_source]
-    hashes = {
-        '1': SHA256.new(message),
-        '2': SHA384.new(message),
-        '3': SHA512.new(message)
-    }
     h = hashes[hash_chosen]
     signature = pkcs1_15.new(key).sign(h)
     # print(signature)
@@ -21,11 +16,6 @@ def sign_pkcs1(hash_chosen, key_source): #signer for Rsa PKCS#1 v1.5
 
 def verify_pkcs1(hash_chosen, key, signature):
     try:
-        hashes = {
-            '1': SHA256.new(message),
-            '2': SHA384.new(message),
-            '3': SHA512.new(message)
-        }
         h = hashes[hash_chosen]
         key = key.publickey()
         pkcs1_15.new(key).verify(h, signature)
@@ -35,7 +25,17 @@ def verify_pkcs1(hash_chosen, key, signature):
 
 
 message = b'To be signed'
+hashes = {
+    '1': SHA256.new(message),
+    '2': SHA384.new(message),
+    '3': SHA512.new(message)
+}
 
 if __name__ == "__main__":
-    key, signature = sign_pkcs1('1', 'generate')
-    verify_pkcs1('1', key, signature)
+    a = ""
+    match a:
+        case "":
+            key, signature = sign_pkcs1('1', 'generate')
+            verify_pkcs1('1', key, signature)
+        case "1":
+            pass

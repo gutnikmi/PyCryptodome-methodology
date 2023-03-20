69b44526ad0f8389d8f6ad973f112d33936f9c68
Сapy <gutnikmi@mail.ru>
2023-03-20 21:12:11 +0300

    working on main

--- code/Lib_test.py
+++ code/Lib_test.py
@@ -3,21 +3,23 @@ from Crypto.Hash import SHA256, SHA384, SHA512
 from Crypto.PublicKey import RSA, ECC, DSA
 
 key = RSA.generate(2048)
-f = open('mykey.pem', 'wb')
-f.write(key.export_key('PEM'))
-f.close()
+with open('mykey.pem', 'wb') as f:
+    f.write(key.export_key('PEM'))
 
-f = open('mykey.pem', 'r')
-key = RSA.import_key(f.read())
-print(key)
-# message = b'blah'
-# key = RSA.generate(2048)
-# h = SHA256.new(message)
-# signature = pkcs1_15.new(key).sign(h)
-# print(signature)
-#
-# try:
-#     pkcs1_15.new(key).verify(h, signature)
-#     print("The signature is valid.")
-# except (ValueError, TypeError) as e:
-#     print("The signature is not valid.", e)
+with open('mykey.pem', 'r') as f:
+    key = RSA.import_key(f.read())
+
+message = b'blah'
+h = SHA256.new(message)
+signature = pkcs1_15.new(key).sign(h)
+with open('signature.txt', 'wb') as f:
+    f.write(signature)
+
+with open('signature.txt', 'rb') as f:
+    imp_sign = f.read()
+
+try:
+    pkcs1_15.new(key).verify(h, imp_sign)
+    print("The signature is valid.")
+except (ValueError, TypeError) as e:
+    print("The signature is not valid.", e)

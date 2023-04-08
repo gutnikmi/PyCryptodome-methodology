c4d47e5b4f90520e9706250184df2ca6f08055c2
gutnikmi <gutnikmi@mail.ru>
2023-04-08 15:52:52 +0300

    benchmark files fully working

--- code/Benchmark.py
+++ code/Benchmark.py
@@ -1,19 +1,56 @@
 from Signer import sign_rsa_v1_5, sign_rsa_pss, sign_eddsa, sign_dsa, sign_ecdsa, sign_pure_eddsa
 from Signer import verify_rsa_v1_5, verify_rsa_pss, verify_eddsa, verify_dsa, verify_ecdsa, verify_pure_eddsa
 import time
+import csv
 
 signers = [sign_rsa_v1_5, sign_rsa_pss, sign_dsa, sign_ecdsa]
 verifiers = [verify_rsa_v1_5, verify_rsa_pss, verify_dsa, verify_ecdsa]
+n_h_s = [sign_eddsa, sign_pure_eddsa]
+n_h_v = [verify_eddsa, verify_pure_eddsa]
 hashes = ['1', '2', '3']
-hash_type = lambda hash: "SHA256" if hash == '1' else "SHA384" if hash == '2' else "SHA512"
-for id, sign in enumerate(signers):
-    for hash in hashes:
-        print(verifiers[id].__name__.replace('verify_', ""), "with", hash_type(hash))
+result = [['algorithm', 'hash', 'sign', 'verify', 'total']]
+alg = []
+modes = ['import', 'generate']  # , 'generate'
+# hash_type = lambda hash: "SHA256" if hash == '1' else "SHA384" if hash == '2' else "SHA512"
+for mode in modes:
+    print(mode)
+    for id, sign in enumerate(signers):
+        for hash in hashes:
+            # print(verifiers[id].__name__.replace('verify_', ""), "with", (lambda hash: "SHA256" if hash == '1' else "SHA384" if hash == '2' else "SHA512")(hash))
+            alg.append(verifiers[id].__name__.replace('verify_', ""))
+            alg.append((lambda hash: "SHA256" if hash == '1' else "SHA384" if hash == '2' else "SHA512")(hash))
+            start_t = time.time()
+            sign(hash, mode, b'To be signed', 'm')
+            sign_t = time.time() - start_t
+            pre_ver = time.time()
+            verifiers[id](hash, None, None, b'To be signed', 'm')
+            verify_time = time.time() - pre_ver
+            total_time = time.time() - start_t
+            alg.append(f'{sign_t:.4f}')
+            alg.append(f'{verify_time:.4f}')
+            alg.append(f'{total_time:.4f}')
+            result.append(alg)
+            alg = []
+            # print("time to sign: ", f'{sign_t:.4f}', "time to verify:", f'{verify_time:.4f}', "time_total:", f'{total_time:.4f}')
+    for id, sign in enumerate(n_h_s):
+        alg.append(n_h_v[id].__name__.replace('verify_', ""))
+        alg.append('-')
         start_t = time.time()
-        sign(hash, 'import', b'To be signed', 'm')
+        sign('', mode, b'To be signed', 'm')
         sign_t = time.time() - start_t
         pre_ver = time.time()
-        verifiers[id](hash, None, None, b'To be signed', 'm')
+        n_h_v[id]('', None, None, b'To be signed', 'm')
         verify_time = time.time() - pre_ver
         total_time = time.time() - start_t
-        print("time to sign: ", f'{sign_t:.4f}', "time to verify:", f'{verify_time:.4f}', "time_total:", f'{total_time:.4f}')
+        alg.append(f'{sign_t:.4f}')
+        alg.append(f'{verify_time:.4f}')
+        alg.append(f'{total_time:.4f}')
+        result.append(alg)
+        alg = []
+    # for res in result:
+    #     print(res)
+    with open('{0}.csv'.format(mode), 'w', newline='') as myFile:
+        writer = csv.writer(myFile, delimiter=';')
+        writer.writerows(result)
+        result = []
+        print("done")
